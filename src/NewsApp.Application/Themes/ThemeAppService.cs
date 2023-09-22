0509c439ce1e70cf9e8af084b5d31830f4c2a4e1
Enzo Tanga <enzo.tanga@onetree.com>
2023-09-22 19:54:19 -0300

    simplicación de metodo de listado de temas

--- src/NewsApp.Application/Themes/ThemeAppService.cs
+++ src/NewsApp.Application/Themes/ThemeAppService.cs
@@ -9,12 +9,27 @@ using Volo.Abp.Domain.Repositories;
 
 namespace NewsApp.Themes
 {
-    public class ThemeAppService : CrudAppService<Theme,ThemeDto,int>, IThemeAppService 
+    public class ThemeAppService : NewsAppAppService, IThemeAppService 
     {
-        public ThemeAppService(IRepository<Theme, int> repository)
-            : base(repository)
-        {
+        private readonly IRepository<Theme, int> _repository;
 
+        public ThemeAppService(IRepository<Theme, int> repository)            
+        {
+            _repository = repository;
+        }
+
+        public async Task<ICollection<ThemeDto>> GetThemesAsync()
+        {
+            var themes = await _repository.GetListAsync();
+
+            return ObjectMapper.Map<ICollection<Theme>, ICollection<ThemeDto>>(themes);
+        }
+
+        public async Task<ThemeDto> GetThemesAsync(int id)
+        {
+            var theme = await _repository.GetAsync(id);
+
+            return ObjectMapper.Map<Theme, ThemeDto>(theme);
         }
     }
 }
